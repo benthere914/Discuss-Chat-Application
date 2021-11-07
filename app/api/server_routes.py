e8fd4f3cd54f824facbfd44d101e1b45ad2f0f8c
Jack Percival <jackpercival7@gmail.com>
2021-11-07 07:37:45 -0800

    Server API organization done

---
+++ app/api/server_routes.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+# from app.models import User
+
+server_routes = Blueprint('servers', __name__)
+
+# Get all servers. Maybe used for the search functionality
+# (or perhaps to show a random assortment of servers on the guild-discovery page)
+@server_routes.route('/')
+@login_required
+def all_servers():
+    return "All servers"
+
+
+# Update a Server name
+@server_routes.route('/<int:serverId>', methods=['PATCH'])
+@login_required
+def update_server(serverId):
+    return "Updated server name"
+
+
+# Delete a Server
+@server_routes.route('/<int:serverId>', methods=['DELETE'])
+@login_required
+def delete_servers(serverId):
+    return "Delete a server"
+
+
+# Get all channels for a single server
+@server_routes.route('/<int:serverId>/channels')
+@login_required
+def all_servers(serverId):
+    return "Get channels for a server"
+
+
+# Add a channel to a server
+@server_routes.route('/<int:serverId>/channels', methods=['POST'])
+@login_required
+def all_servers(serverId):
+    return "Added a channel"
+
+
+# Get all memebrs for a single server
+@server_routes.route('/<int:serverId>/members')
+@login_required
+def all_servers(serverId):
+    return "Get server members"
+
+
+# Add a member to a server
+@server_routes.route('/<int:serverId>/members', methods=['POST'])
+@login_required
+def all_servers(serverId):
+    return "Added a member to the server"
