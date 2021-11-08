cd7c0b9a995eafbc0c0a575f4d6dfaf87d446298
Jack Percival <jackpercival7@gmail.com>
2021-11-08 10:38:49 -0800

    Some seeders done

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .servers import seed_servers, undo_servers
+from .channels import seed_channels, undo_channels
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,11 +12,16 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_servers()
+    seed_channels()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_servers()
     undo_users()
+    undo_channels()
+
     # Add other undo functions here
