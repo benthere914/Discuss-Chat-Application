9427e46e22daaf5fcf06d1fd2849463bad9458ef
Jack Percival <jackpercival7@gmail.com>
2021-11-10 12:38:52 -0800

    search bar added

--- react-app/src/components/guild/guildDiscovery.js
+++ react-app/src/components/guild/guildDiscovery.js
@@ -4,8 +4,22 @@ import './guildDiscovery.css'
 const GuildDiscovery = () => {
   return (
       <div className="mainSearchContent">
-          <div className="topSearchContent">
-
+          <div className="topSearchBG">
+            <div className="topSearchContent">
+              <h3>Find your community on Discuss</h3>
+              <h5>From gaming, to music, to learning, there's a place for you.</h5>
+              <div className="searchGuildForm">
+                <form>
+                  <input
+                  className="searchInput"
+                  type="text"
+                  placeholder="Explore communities"
+                  />
+                  <button>Search</button>
+                </form>
+              </div>
+            </div>
+            <img className="searchBGImage" src="https://res.cloudinary.com/dt8q1ngxj/image/upload/v1636571660/Discuss/guildDiscovery_joyfnj.png" alt ="Search BG"/>
           </div>
       </div>
   );
