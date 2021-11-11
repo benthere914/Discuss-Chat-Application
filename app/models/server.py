8217b6cc5efab33cab97cf92902136381022b3f7
valeriareynososf <valeriayearup@gmail.com>
2021-11-11 15:06:47 -0800

    server name not unique

--- app/models/server.py
+++ app/models/server.py
@@ -11,7 +11,7 @@ class Server(db.Model):
     __tablename__ = 'servers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40), nullable=False, unique=True)
+    name = db.Column(db.String(40), nullable=False, unique=False)
     description = db.Column(db.String(500), nullable=True, unique=False)
     owner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, unique=False)
     icon = db.Column(db.String(1000), nullable=False, unique= False)
