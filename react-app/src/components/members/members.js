fc9e89f04ff9b5f29b978afa2f6d177299e70c94
Benjamin Rose <benthere914@gmail.com>
2021-11-11 16:27:11 -0600

    bug fix

--- react-app/src/components/members/members.js
+++ react-app/src/components/members/members.js
@@ -3,9 +3,11 @@ import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import { loadServerMembers } from "../../store/members"
 import { useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
 import './members.css'
 
 const Members = () => {
+    const params = useParams()
     const shortenUsername = (username, num=15) => {
         if (username?.length >= num){
             username = username.slice(0, num)
@@ -16,19 +18,19 @@ const Members = () => {
     const dispatch = useDispatch()
     const members = useSelector(state => Object.values(state.members))
     useEffect(() => {
-        dispatch(loadServerMembers(1))
+        dispatch(loadServerMembers(params?.serverId)).then(() => {console.log(members, params?.serverId)})
 
-    }, [])
+    }, [params?.serverId])
 
     return (
     <>
         <div className='membersTab'>
-            <p className='membersTitle'>{`Members - ${members.length}`}</p>
+            <p className='membersTitle'>{`Members - ${members?.length}`}</p>
             <ul>{members?.map((e) => (
                 <li key={e.username}>
-                    <img clasname='memberIcon' src={`https://www.gravatar.com/avatar/${md5(e.email)}`} alt={e.username}></img>
-                    <i className='fas fa-circle memberActive' style={e.online?{color: 'green'}:{color:'red'}}></i>
-                    <p className='memberUsername'>{shortenUsername(e.username)}</p>
+                    <img clasname='memberIcon' src={`https://www.gravatar.com/avatar/${md5(e?.email)}`} alt={e?.username}></img>
+                    <i className='fas fa-circle memberActive' style={e?.online?{color: 'green'}:{color:'red'}}></i>
+                    <p className='memberUsername'>{shortenUsername(e?.username)}</p>
                 </li>))}
             </ul>
         </div>
