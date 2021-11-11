01fc537199a3acd1ade10fe862b688bb38bb09da
Benjamin Rose <benthere914@gmail.com>
2021-11-11 15:39:22 -0600

    added more locations to have members

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -51,6 +51,7 @@ function App() {
             <div className="channelBar">
               <div className="emptyChannels">Select a Server</div>
               <ProfileBar/>
+              <Members/>
             </div>
           </div>
         </Route>
@@ -81,6 +82,7 @@ function App() {
             <div className="channelBar">
               <ChannelsContainer />
               <ProfileBar/>
+              <Members/>
             </div>
           <Messages/>
           </div>
