8d25034488a39fb92cbca0e143942abcdb418e74
Jack Percival <jackpercival7@gmail.com>
2021-11-10 16:14:57 -0800

    More styling

--- react-app/src/components/guild/singleSearch.js
+++ react-app/src/components/guild/singleSearch.js
@@ -9,16 +9,13 @@ function ServerCard({user, server, userServers}) {
 
     const checkIfUserInServer = () => {
         let alreadyJoined = false;
-        console.log(userServers)
         for (let [key, value] of Object.entries(userServers)) {
-            console.log(value.id)
             if (value.id === server.id) {
                 alreadyJoined = true;
                 break;
             }
         }
 
-        console.log(alreadyJoined)
         return alreadyJoined;
     }
 
@@ -34,17 +31,18 @@ function ServerCard({user, server, userServers}) {
             ) : (
                 <div
                     className="singleServerNoImage"
-                    // Remove this and change to cute logo
-                    style={{ backgroundImage: `url(https://res.cloudinary.com/dt8q1ngxj/image/upload/v1634139516/soccr/barcabadge_bzcfgk.png)` }}>
+                    style={{ backgroundImage: `url(https://res.cloudinary.com/dt8q1ngxj/image/upload/v1636589623/Discuss/discussIcon_tkg8y9.png)` }}>
                 </div>
             )}
-            <h6>{server.name}</h6>
-            <p>{server.description}</p>
-            {alreadyJoined? (
-                <div>Leave Server</div>
-            ) : (
-                <div>Join Server</div>
-            )}
+            <div className="bottomServerCard">
+                <h6>{server.name}</h6>
+                <p>{server.description}</p>
+                {alreadyJoined? (
+                    <div>Leave Server</div>
+                ) : (
+                    <div>Join Server</div>
+                )}
+            </div>
         </div>
     )
 }
